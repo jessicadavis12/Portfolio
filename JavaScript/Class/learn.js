141194834be0851079ecec5b23fdea391e1c2772
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-06-24 19:05:03 -0500

    revtwo

---
+++ JavaScript/Class/learn.js
@@ -0,0 +1,22 @@
+//function greetings1(){
+  //  console.log("Hey guys, how's it going?");
+    //console.log("Where is the party at?");
+    //console.log("Did anybody bring spinach dip?")
+//                      }
+//greetings1();
+
+
+//function greetings(age) {
+  //  return "So I understand you are " +age+" years old";
+//}
+//console.log(greetings(27))
+
+function fullGreetings(age, name){
+    return "Hi " +name+ ", I believe you are " +age+ "years old.";
+}
+console.log(fullGreetings(27, "Jenny"));
+
+
+function area(length, width){
+    return (length*width)}
+console.log(area(4, 5))
