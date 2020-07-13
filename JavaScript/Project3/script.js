548ad57581f3bd5e12fd49986d231c36dcd5302f
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-07-13 11:12:05 -0500

    initial push

--- JavaScript/Project3/script.js
+++ JavaScript/Project3/script.js
@@ -1,11 +1,15 @@
-var num=[];
-num[0]= Number(prompt("Enter a number: ";));
-num[1]= Number(prompt("Enter a number: ";));
-num[2]= Number(prompt("Enter a number: ";));
 
+function populatearray(){
+    var num=[];
+    num[0]= Number(prompt("Enter a number: ";));
+    num[1]= Number(prompt("Enter a number: ";));
+    num[2]= Number(prompt("Enter a number: ";));
+    function additup(){};
+}
 
 
 function additup(){
+    
 var arraysum = 0;
 arraylen = num.length;
 for (i=0; i<arraylen; i++){
