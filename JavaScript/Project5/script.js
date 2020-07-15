25e4f5954be010a0057b03193b60383539f5653f
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-07-15 15:05:20 -0500

    rev 4

--- JavaScript/Project5/script.js
+++ JavaScript/Project5/script.js
@@ -1,24 +1,25 @@
-var menu=["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg"];
+var menuArray=["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg"];
 
 var i = 0;
 
-document.getElementById("next").addEventListener("click", 
-    function nextImage(i){
-        if (i<=menu.length-1){
-             i++
-         }else{
-            i = 0;
-            }; 
+ 
+function nextImage(){
+    if (i<menuArray.length-1){
+         i++
+    }else{
+        i = 0;
+        }; 
+    document.querySelector("img").setAttribute("src", menuArray[i]);
     };
-    document.getElementById("cookies")="url('" + menu[i] +"')";
-)
+    
 
-document.getElementById("last").addEventListener("click", 
- function prevImage(i){
-     if (i=0){
-        i=6;
-     }else{
+
+ function prevImage(){
+    if (i=0){
+        i=menuArray.length-1
+    }else{
          i=i-1
      }
+    document.querySelector("img").setAttribute("src", menuArray[i]);
  }
- document.getElementById("cookies")="url('" + menu[i] +"')";
+ 
