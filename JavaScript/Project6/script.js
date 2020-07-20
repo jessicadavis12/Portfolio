0582867a901e019e76f086f4b3fa03d9ec8bc587
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-07-20 14:39:50 -0500

    getting help

--- JavaScript/Project6/script.js
+++ JavaScript/Project6/script.js
@@ -1,9 +1,8 @@
 function changeColor(){
-var backgroundColors=["#4FC3F7", "#29B6F6", '#03A9F4', '#039BE5', '#0288D1', '#0277BD', '#01579B', '#9575CD', '#7E57C2', '#673AB7', '#5E35B1', '#512DA8', '#4527A0', '#311B92', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E', '#64B5F6', '#42A5F5', '#2196F3', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1'];
+var backgroundColors=['#4FC3F7', '#29B6F6', '#03A9F4', '#039BE5', '#0288D1', '#0277BD', '#01579B', '#9575CD', '#7E57C2', '#673AB7', '#5E35B1', '#512DA8', '#4527A0', '#311B92', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E', '#64B5F6', '#42A5F5', '#2196F3', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1'];
 var i= 1;
-var back = document.body.style.background;
-back = backgroundColors[i];
-i=i+1;
+document.querySelector("body").style.background=(backgroundColors[i]);
+i++;
 if(i> backgroundColors.length-1){
     i=1;
 }
