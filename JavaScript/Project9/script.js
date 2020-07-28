189ece47b5a8023fbd43e50fa04f51fc7e3fe771
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-07-28 11:40:22 -0500

    recipe restructure

--- JavaScript/Project9/script.js
+++ JavaScript/Project9/script.js
@@ -1,20 +1,26 @@
-var page1={
+//untill we learn the backend I just need some variable entries 
+//array of objects for selection and addition 
+//buttons need to ids that a function can sort through to find the match 
+//to display 
+
+var recipieBook=[
+    {
     title:"Egg Salad",
     serving: 4,
     calories: 300,
     Recipe:"Boil the eggs for 15",
     ul: "ingredients for shoping list",
     pictu: "future img src",
-    }
-var page2={
+    },
+    {
     title:"Chicken Salad",
     serving: 2,
     calories: 700,
     Recipe:"Grill the chicken for",
     ul: "ingredients for shoping list",
     pictu: "future img src",
-    }
- var page3={
+    },
+    {
     title:"Garden Salad",
     serving: 8,
     calories: 500,
@@ -22,44 +28,57 @@ var page2={
     ul: "ingredients for shoping list",
     pictu: "future img src",
     }
+]
 
-var recipieBook=["page1", "page2", "page3"]
 //create a function that selects the correct array entry
 //based on user check box choice
+
+
+ 
+//the website is flasing the choice - on project 2 I used event to solve 
+//object constructor for new recipe/ want to add a list component with
+//another object constructor- ie website will render a shopping list
+
+
+//to solve the display issue, and DRY this up- I need to create a function that will display one selected object- and that i can change based on user input.  Need to connect the drop down selection with displaying the selected- also I don't think that the var i syntex is ok  re
+ 
+var i = $("recipe").val("option value")
+
+function displaySelected(){
+    var displayTitle= recipeBook[i].title;
+    var displayServing= recipeBook[i].serving;
+    var displayCalories= recipeBook[i].calories;
+    var displayRecipe= recipeBook[i].Recipe;
+    var displayUL=recipeBook[i].ul;
+    var displayPictu=recipeBook[i].pictu;
+    $("title").text(displayTitle);
+    $("servings").text(displayTitle);
+    $("calories").text(displayCalories);
+    $("rec").text(displayRecipe);
+    $("ing").text(displayUL);
+    $("photo").img(displayPictu);
+}
+
+//this doesn't work with the field entry from home page 
+
+
+function newRecipe(title, serving, calories, Recipe, ul, pictu){
+    this.newTitle = title;
+    this.newServing = serving;
+    this.newCalories = calories;
+    this.newRecipe = Recipe;
+    this.newUl = ul;
+    this.newPictu = pictu;
+
+}
+//now how do I add a button to select a newly added recipe?
+//to solve after I fix the display and maybe add some style
+//both add and remove class 
  
 
-
-function egg(){
-    document.getElementById("title").innerHTML= page1.title;
-    document.getElementById("servings").innerHTML= page1.serving;
-    document.getElementById("calories").innerHTML= page1.calories;
-    document.getElementById("rec").innerHTML= page1.Recipe;
-    document.getElementById("ing").innerHTML= page1.ul;
-    document.getElementById("photo").innerHTML= page1.pictu;
-}
-
-function chicken(){
-    document.getElementById("title").innerHTML= page2.title;
-    document.getElementById("servings").innerHTML= page2.serving;
-    document.getElementById("calories").innerHTML= page2.calories;
-    document.getElementById("rec").innerHTML= page2.Recipe;
-    document.getElementById("ing").innerHTML= page2.ul;
-    document.getElementById("photo").innerHTML= page2.pictu;
-}
-
-function Garden(){
-    document.getElementById("title").innerHTML= page3.title;
-    document.getElementById("servings").innerHTML= page3.serving;
-    document.getElementById("calories").innerHTML= page3.calories;
-    document.getElementById("rec").innerHTML= page3.Recipe;
-    document.getElementById("ing").innerHTML= page3.ul;
-    document.getElementById("photo").innerHTML= page3.pictu;
-}
-
-
-
-
 //function to display recipe stored in array 
+//need a function to display all of the titles that calls the rest of the 
+//object-  Recipe Book is an object of recipe objects  
 
 
 // would like to add a new field in the browser
