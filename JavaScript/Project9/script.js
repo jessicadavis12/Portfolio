6deee9d6e3a3cc08b882122197c2b06c0a7e6ba3
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-07-28 15:08:02 -0500

    project 9 JS

--- JavaScript/Project9/script.js
+++ JavaScript/Project9/script.js
@@ -1,7 +1,6 @@
 //untill we learn the backend I just need some variable entries 
 //array of objects for selection and addition 
-//buttons need to ids that a function can sort through to find the match 
-//to display 
+
 
 var recipieBook=[
     {
@@ -9,24 +8,19 @@ var recipieBook=[
     serving: 4,
     calories: 300,
     Recipe:"Boil the eggs for 15",
-    ul: "ingredients for shoping list",
-    pictu: "future img src",
     },
     {
     title:"Chicken Salad",
     serving: 2,
     calories: 700,
     Recipe:"Grill the chicken for",
-    ul: "ingredients for shoping list",
-    pictu: "future img src",
+    
     },
     {
     title:"Garden Salad",
     serving: 8,
     calories: 500,
     Recipe:"Wash and cut",
-    ul: "ingredients for shoping list",
-    pictu: "future img src",
     }
 ]
 
@@ -42,34 +36,32 @@ var recipieBook=[
 
 //to solve the display issue, and DRY this up- I need to create a function that will display one selected object- and that i can change based on user input.  Need to connect the drop down selection with displaying the selected- also I don't think that the var i syntex is ok  re
  
-var i = $("recipe").val("option value")
 
-function displaySelected(){
+
+function displaySelected(recipeBook){
+    var i = $("select option:selected").val();
     var displayTitle= recipeBook[i].title;
     var displayServing= recipeBook[i].serving;
     var displayCalories= recipeBook[i].calories;
     var displayRecipe= recipeBook[i].Recipe;
     var displayUL=recipeBook[i].ul;
     var displayPictu=recipeBook[i].pictu;
-    $("title").text(displayTitle);
-    $("servings").text(displayTitle);
-    $("calories").text(displayCalories);
-    $("rec").text(displayRecipe);
-    $("ing").text(displayUL);
-    $("photo").img(displayPictu);
+    $("title2").html(displayTitle.text());
+    $("servings2").html(displayTitle.text());
+    $("calories2").html(displayCalories.text());
+    $("rec").html(displayRecipe.text);
+  
 }
 
-//this doesn't work with the field entry from home page - will link by id 
+//this doesn't work with the field entry from home page - 
 
 
-function newRecipe($("title","ssize", "cal", "recipeent", 'ul', "Picture" ).val()){
-    this.newTitle = title;
-    this.newServing = serving;
-    this.newCalories = calories;
-    this.newRecipe = Recipe;
-    this.newUl = ul;
-    this.newPictu = pictu;
-
+function newRecipe(){
+    this.newTitle = $("title").val;
+    this.newServing = $("#ssize").val;
+    this.newCalories = $("#cal").val;
+    this.newRecipe = $("#recipent").val;
+   
 }
 //now how do I add a button to select a newly added recipe?
 //to solve after I fix the display and maybe add some style
@@ -82,10 +74,7 @@ function newRecipe($("title","ssize", "cal", "recipeent", 'ul', "Picture" ).val(
 
 
 // would like to add a new field in the browser
-// let title= $("#title").value;
-// let ssize=$("#ssize").value;
-// let cal=$("#cal").value;
-// let recipe=$("#recipe").value;
+
 
 // var recipePage = [title, ssize, cal, recipe]
 
