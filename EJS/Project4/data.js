8373146196334b024e4f8e8fbd43b2e4e766a22d
jessicadavis12 <mrs.jessica.davis36@gmail.com>
2020-08-11 15:28:46 -0500

    EJS Project4

---
+++ EJS/Project4/data.js
@@ -0,0 +1,18 @@
+var people = [
+    {
+        name: "George",
+        description: "Lorem ipsum",
+        age: "24"
+
+    },
+    {
+        name: "Jack",
+        description: "Lorem ipsum",
+        age: "32"
+    },
+    {
+        name: "Brenda",
+        description: "Loreum Eplison",
+        age: "46"
+    }
+] 
